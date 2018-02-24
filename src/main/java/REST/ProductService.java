0498b672bc8aff64204b72a84aecad7faad8679d
alinoleumm <albina.li@nu.edu.kz>
2018-02-24 18:42:52 +0600

    Filters fixed

--- src/main/java/REST/ProductService.java
+++ src/main/java/REST/ProductService.java
@@ -1,7 +1,7 @@
 package REST;
 
 import Models.Product;
-import Utils.Filterer;
+//import Utils.Filterer;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -45,44 +45,104 @@ public class ProductService {
         return b.build();
     }
 
+//    /**
+//     * Filtering path
+//     */
+//
+//    @GET
+//    @Path("/filter")
+//    public Response mainFilterer(@QueryParam("title") String title,
+//                                      @QueryParam("category") String category,
+//                                      @QueryParam("type") String type,
+//                                      @QueryParam("price") double price,
+//                                      @QueryParam("units") String units,
+//                                      @QueryParam("email") String email) {
+//        ArrayList<Product> reqProds = new ArrayList<Product>();
+//        Filterer filterer = new Filterer(products);
+//        // By title
+//        reqProds.addAll(filterer.getProductByTitle(title));
+//        // Category
+//        reqProds.addAll(filterer.getProductByCategory(category));
+//        // Type, price units
+//        reqProds.addAll(filterer.getProductByPrice(type, price, units));
+//        // Email
+//        reqProds.addAll(filterer.getProductByUser(email));
+//
+//        ArrayList<Product> uniqueList = new ArrayList<>(new HashSet<Product>(reqProds));
+//        return Response.ok(new Gson().toJson(uniqueList)).build();
+//    }
+
+
     /**
-     * Filtering path
+     * Filters
      */
 
     @GET
-    @Path("/filter")
-    public Response mainFilterer(@QueryParam("title") String title,
-                                      @QueryParam("category") String category,
-                                      @QueryParam("type") String type,
-                                      @QueryParam("price") double price,
-                                      @QueryParam("units") String units,
-                                      @QueryParam("email") String email) {
-        ArrayList<Product> reqProds = new ArrayList<Product>();
-        Filterer filterer = new Filterer(products);
-        // By title
-        reqProds.addAll(filterer.getProductByTitle(title));
-        // Category
-        reqProds.addAll(filterer.getProductByCategory(category));
-        // Type, price units
-        reqProds.addAll(filterer.getProductByPrice(type, price, units));
-        // Email
-        reqProds.addAll(filterer.getProductByUser(email));
-
-        ArrayList<Product> uniqueList = new ArrayList<>(new HashSet<Product>(reqProds));
-        return Response.ok(new Gson().toJson(uniqueList)).build();
-    }
-
-    @GET
-    @Path("{ id : [A-Za-z0-9_]+}")
-    public String getProduct(@PathParam("id") String id) {
-        System.out.println(id);
-        for (Product p : products) {
-            if (p.getID().equals(id)) {
-                return p.toJSON();
+    @Path("/filters")
+    public Response getProductByFilters(@QueryParam("title") String title,
+                                        @QueryParam("price") double price,
+                                        @QueryParam("category") String category) {
+        System.out.println(title);
+        System.out.println(price);
+        System.out.println(category);
+        ArrayList<Product> reqProds = new ArrayList<>();
+        if(title==null && category==null) {
+            for (Product p : products) {
+                if (p.getPrice()<=price) {
+                    reqProds.add(p);
+                }
+            }
+        } else if (title==null && category!=null) {
+            for (Product p : products) {
+                if (p.getPrice()<=price && p.getCategory().equals(category)) {
+                    reqProds.add(p);
+                }
+            }
+        } else if (title!=null && category==null) {
+            for (Product p : products) {
+                if (p.getPrice()<=price && p.getTitle().toLowerCase().contains(title.toLowerCase())) {
+                    reqProds.add(p);
+                }
+            }
+        } else {
+            for (Product p : products) {
+                if (p.getPrice()<=price && p.getCategory().equals(category) && p.getTitle().toLowerCase().contains(title.toLowerCase())) {
+                    reqProds.add(p);
+                }
             }
         }
-        return "";
+        return Response.ok(new Gson().toJson(reqProds)).build();
     }
+
+    /**
+     * Retrieve products owned by particular user (change email for user id)
+     **/
+
+    @GET
+    @Path("/myproducts")
+    public Response getProductByUser(@QueryParam("email") String email) {
+        ArrayList<Product> reqProds = new ArrayList<>();
+        if (email != null) {
+            for (Product p : products) {
+                if (p.getAuthorEmail().equals(email)) {
+                    reqProds.add(p);
+                }
+            }
+        }
+        return Response.ok(new Gson().toJson(reqProds)).build();
+    }
+
+//    @GET
+//    @Path("{ id : [A-Za-z0-9_]+}")
+//    public String getProduct(@PathParam("id") String id) {
+//        System.out.println(id);
+//        for (Product p : products) {
+//            if (p.getID().equals(id)) {
+//                return p.toJSON();
+//            }
+//        }
+//        return "";
+//    }
 
     @POST
     @Consumes("application/json")
